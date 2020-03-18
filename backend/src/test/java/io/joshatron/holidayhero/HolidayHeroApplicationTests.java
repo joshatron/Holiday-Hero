de00006d944f7235ff696e790ad2a4d7fd7aa196
Joshua Leger <legermjoshua@gmail.com>
2020-03-17 22:16:24 -0700

    Initialized spring boot application

---
+++ backend/src/test/java/io/joshatron/holidayhero/HolidayHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package io.joshatron.holidayhero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayHeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
